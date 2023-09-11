1be862fe2ac93d89ddc69439e76c3d6d70b14acf
Verônica Helena <desenvolveronica@gmail.com>
2023-09-11 13:44:30 -0300

    Conexão banco SQL SERVER

--- LoginC#/DAL/LoginDaoComandos.cs
+++ LoginC#/DAL/LoginDaoComandos.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +10,30 @@ namespace LoginC_.DAL
 {
     internal class LoginDaoComandos
     {
-        public bool tem;
-        public String mensagem;
+        public bool tem = false;
+        public String mensagem = ""; //se a variável estiver vazia é pq está tudo OK
+        SqlCommand cmd = new SqlCommand();
+        Conexao con = new Conexao();
+        SqlDataReader dr;
         public bool verificarLogin(String login, String senha)
         {
+            cmd.CommandText = "select * from logins where email = @login and senha = @senha";
+            cmd.Parameters.AddWithValue("@login", login);
+            cmd.Parameters.AddWithValue("@senha", senha);
+
+            try{
+                cmd.Connection = con.conectar();
+                dr = cmd.ExecuteReader();
+                if (dr.HasRows) //verifica se foi encontrado alguma linha correspondente no BD
+                {
+                    tem = true; 
+                }
+            }
+            catch (SqlException){
+
+                this.mensagem = "Erro com Banco de dados";
+            }
+
             //método com os comandos SQL para verificar se tem no banco
             return tem;
         }
