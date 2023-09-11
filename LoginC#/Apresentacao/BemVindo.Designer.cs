4383316aa6b3a5c9461ce6fc55f60b9c0688dd18
Verônica Helena <desenvolveronica@gmail.com>
2023-09-11 11:46:59 -0300

    Ajustes

--- LoginC#/Apresentacao/BemVindo.Designer.cs
+++ LoginC#/Apresentacao/BemVindo.Designer.cs
@@ -36,10 +36,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(300, 128);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(206, 25);
+            this.label1.Size = new System.Drawing.Size(223, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "SEJA VEM VINDO !!";
             this.label1.Click += new System.EventHandler(this.label1_Click);
